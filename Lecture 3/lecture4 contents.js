147a6bfd1f24bff0be3658e2db11e58858720bcf
Hina Nadeem <hi89141na@gmail.com>
2024-12-24 01:34:32 +0500

    Week 3

---
+++ Lecture 3/lecture4 contents.js
@@ -0,0 +1,109 @@
+/***1. To write some content on web page ***/
+// document.write('hello2');
+
+/*** 2. Use html in script ***/
+// document.write('<b>hello2</b>');
+
+/*** 3. variables ***/
+
+// var x = "john wick";
+// var x = "jason statham";
+
+// let y = "var 2";
+// let y = "var 3"; // let does allow re declaratoin
+
+// const z = "abc";
+// z = 123;
+
+// document.write(z);
+
+/*** 4. Data types  ***/
+////// string, number, boolean, object, undefined
+// var x;
+// x = "a";
+// x = 1.5;
+// x = true;
+// x = ["1", "name", "true"];
+// x = {"name":"abc", "id":1};
+// document.write(typeof(x) );
+
+/*** 5. comparison operator  ***/
+// var a = 55;
+// var b = 54;
+
+// console.log(a === b);
+
+/*** 6. if statement   ***/
+// if(6 === 6){
+// 	console.log("both are equal");
+// }
+// else{
+// 	console.log("both are not equal");
+// }
+
+/*** 7. Switch Statement   ***/
+// var x = 3;
+// switch(x){
+// 	case 1:
+// 		console.log('value is 1');
+// 		break;
+// 	case 2:
+// 		console.log('value is 2');
+// 		break;
+// 	default:
+// 		console.log('value is something else');
+// }
+
+/*** 8. alert box   ***/
+// alert('this is a alert box');
+
+/*** 9. confirm box   ***/
+//var ans = confirm('this is a confirm box');
+//console.log(ans);
+
+/*** 10. prompt box   ***/
+ //var ans = prompt('what do you want to study ??');
+ //console.log(ans);
+ //console.log(typeof(ans))
+
+/*** 11. Functions   ***/
+
+// abc();
+
+// function abc(){
+// 	alert('hello from function ' );
+// }
+
+/*** 12. Events   ***/
+// <!-- onclick, ondblclick, oncontextmenu (right click), onmouseenter, onmouseout, onkeypress (in body and form only), onresize, onload  -->
+
+// function fun(){
+// 	alert('hello there');
+// }
+
+/*** 13. while loop   ***/
+
+// var i = 0;
+// while(i < 5){
+// 	document.write('value of i is = ' + i + '<br>');
+// 	i++;
+// }
+
+/*** 14. For loop   ***/
+// for (i = 0; i <= 6; i++){
+// 	document.write('i = ' + i + '<br>');
+// }
+
+/*** 15. Do while loop   ***/
+// let i = 1;
+// do{
+// 	document.write('value of i = ' + i + "<br>");
+// 	i++;
+// }
+// while(i <= 10);
+
+/*** 16. Arrays   ***/
+// var arr = [10,"20",true,null,50];
+// for (var i = 0; i < arr.length; i++) {
+// 	document.write(arr[i] + '<br>');
+// }
